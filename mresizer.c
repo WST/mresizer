ca46fd9df2416d4f432b533e2742a618eaf037c0
WST <admin@jsmart.web.id>
2014-05-15 21:33:37 +0400

    Information messages

--- mresizer.c
+++ mresizer.c
@@ -3,18 +3,27 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
 
 char destination_filename[255];
 
 int main(int argc, char *argv[]) {
 
+	// Output directory
+	mkdir("results", S_IRWXU | S_IRWXG | S_IRWXO);
+
+	// Directory for reading and file entry
 	DIR *directory;
 	struct dirent *entry;
 
+	// Main loop
 	for(directory = opendir("."); (entry = readdir(directory)) != 0;) {
 		if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
 		if(strstr(entry->d_name, ".jpg") != entry->d_name + strlen(entry->d_name) - 4) continue;
 		if(strstr(entry->d_name, ".resized.jpg") == entry->d_name + strlen(entry->d_name) - 12) continue;
+
+		printf("Processing <%s>\n", entry->d_name);
 
 		FILE *file = fopen(entry->d_name, "r");
 		gdImagePtr source = gdImageCreateFromJpeg(file);
@@ -22,14 +31,14 @@ int main(int argc, char *argv[]) {
 		int source_width = gdImageSX(source);
 		int source_height = gdImageSY(source);
 
-		int destination_width = source_width / 8;
-		int destination_height = source_height / 8;
+		int destination_width = 1024;
+		int destination_height = 768;
 
 		gdImagePtr destination = gdImageCreateTrueColor(destination_width, destination_height);
 
 		gdImageCopyResampled(destination, source, 0, 0, 0, 0, destination_width, destination_height, source_width, source_height);
 
-		sprintf(destination_filename, "%s.resized.jpg", entry->d_name);
+		sprintf(destination_filename, "results/%s.resized.jpg", entry->d_name);
 
 		FILE *destination_file = fopen(destination_filename, "w");
 		fseek(destination_file, 0, SEEK_SET);
@@ -40,9 +49,10 @@ int main(int argc, char *argv[]) {
 		gdImageDestroy(source);
 
 		fclose(file);
-
 	}
-
 	closedir(directory);
+
+	printf("Job done!\n");
+
 	return 0;
 }
